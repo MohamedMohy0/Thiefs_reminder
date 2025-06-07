8634420db19a2e3cc35f746c875f21213d11132a
Mohamed Mohy <105385056+MohamedMohy0@users.noreply.github.com>
2025-06-07 15:30:32 +0300

    Update Thiefs.py

--- Thiefs.py
+++ Thiefs.py
@@ -21,20 +21,39 @@ worksheet = sh.sheet1
 
 all_values = worksheet.get_all_values()
 num_rows = len(all_values)
-
-bot_token = os.environ.get('BOT_TOKEN')
-chat_id = os.environ.get('CHAT_ID')
-message = f'There is a new Thief With Number: {num_rows}'
-
-url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-payload = {
-    'chat_id': chat_id,
-    'text': message
-}
-
-response = requests.post(url, data=payload)
-
-if response.status_code == 200:
-    print("Message sent successfully!")
+prev_num_file = 'prev_num.txt'
+with open(prev_num_file, 'r') as f:
+    prev_num = int(f.read().strip())
+if num_rows > prev_num:
+    bot_token = os.environ.get('BOT_TOKEN')
+    chat_id = os.environ.get('CHAT_ID')
+    message = f'There is a new Thief With Number: {num_rows}'
+    
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    
+    response = requests.post(url, data=payload)
+    
+    if response.status_code == 200:
+        print("Message sent successfully!")
+        with open(prev_num_file, 'w') as f:
+            f.write(str(num_rows))
+    else:
+        print(f"Failed to send message: {response.text}")
+    
 else:
-    print(f"Failed to send message: {response.text}")
+    bot_token = os.environ.get('BOT_TOKEN')
+    chat_id = os.environ.get('CHAT_ID')
+    message = f'There is a no Thiefs'
+    
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    
+    response = requests.post(url, data=payload)
+    
