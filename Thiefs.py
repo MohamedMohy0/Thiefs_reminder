10f68ab8448f07689a8b55fc1f0de85fb1903add
Mohamed Mohy <105385056+MohamedMohy0@users.noreply.github.com>
2025-06-07 15:08:56 +0300

    Update Thiefs.py

--- Thiefs.py
+++ Thiefs.py
@@ -1,13 +1,13 @@
+import os
 import requests
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Your local JSON file path
+# قراءة ملف Config.json موجود في نفس المجلد
 SERVICE_ACCOUNT_FILE = 'Config.json'
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# Authenticate with Google Sheets API
 credentials = Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE,
     scopes=SCOPES
@@ -15,18 +15,16 @@ credentials = Credentials.from_service_account_file(
 
 gc = gspread.authorize(credentials)
 
-# Open your Google Sheet by URL or by name
 sheet_url = 'https://docs.google.com/spreadsheets/d/1MsnREiLwMupFv1JqFN1bApRyVeiC9GWahRxtcuyW5CU/edit#gid=0'
 sh = gc.open_by_url(sheet_url)
-
-worksheet = sh.sheet1  # Get the first worksheet
+worksheet = sh.sheet1
 
 all_values = worksheet.get_all_values()
 num_rows = len(all_values)
 
-bot_token = '8124295808:AAHZzbmfpRntxSFVZMAEfLyve8PfaP8EU48'
-chat_id = '1752719138'
-message = f'There is a new Thief With Number:{num_rows}'
+bot_token = os.environ.get('BOT_TOKEN')
+chat_id = os.environ.get('CHAT_ID')
+message = f'There is a new Thief With Number: {num_rows}'
 
 url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 payload = {
