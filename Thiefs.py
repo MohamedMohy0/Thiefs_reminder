2f0a57d67a69e46b08a7ed951f650c519630bd7a
Mohamed Mohy <105385056+MohamedMohy0@users.noreply.github.com>
2025-10-19 16:51:56 +0300

    Update Thiefs.py

--- Thiefs.py
+++ Thiefs.py
@@ -5,7 +5,7 @@ from google.oauth2.service_account import Credentials
 from google.auth.exceptions import RefreshError
 from gspread.exceptions import APIError
 
-
+#
 SERVICE_ACCOUNT_FILE = 'Config.json'
 
 SCOPES = [
@@ -68,4 +68,5 @@ except:
             'text': message
         }
     response = requests.post(url, data=payload)
+
 
